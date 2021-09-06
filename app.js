57ef6ebeb23830b3d1cf064de5f40af0f4e034cb
Md-Alamin-hub <86272640+Md-Alamin-hub@users.noreply.github.com>
2021-09-06 11:27:23 +0600

    new feature add

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
-
-const searchInput = document.getElementById(searchInput);
+const searchButton = () => {
+const searchInput = document.getElementById('search-input');
 const searchText = searchInput.value;
-
-const searchBtn = () => {
-    const url = `https: //www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita`;
+searchInput.value = '';
+const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchText}`;
     fetch(url)
     .then(res => res.json())
-    .then(data => console.log(data))        
+    .then(data => searchDrinks(data.drinks))
+};
+// }
+
+const searchDrinks = drinks => {
+const drinksCard = document.getElementById('cardDrink');
+    drinksCard.textContent = '';
+    drinks.forEach(drink => {
+    const eachDrinkCard = document.createElement('div');
+
+    eachDrinkCard.innerHTML = `
+    <div class="col">
+        <div class="card h-100">
+            <img style = "height: 300px;" src="${drink.strDrinkThumb}" class="card-img-top img-fluid d-block" alt="..." />
+        <div class="card-body">
+            <h4 class="card-title fw-bold text-center"><a class = "text-decoration-none" href="">Name: ${drink.strDrink}</a></h4>
+        </div>
+        <div class = "card-footer text-center">
+        <p>Date: ${drink.dateModified}</p>
+        </div>
+        </div>
+    </div>
+`;
+drinksCard.appendChild(eachDrinkCard);
+
+})
 }
